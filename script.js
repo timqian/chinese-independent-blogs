e5e6a3fa44f885bc0df9e3774ae4dd45d41669e3
timqian <timqian92@qq.com>
2019-10-24 15:26:07 +0800

    finish script

--- script.js
+++ script.js
@@ -1,11 +1,118 @@
 const fs = require('fs');
+const axios = require('axios');
+const markdownTable = require('markdown-table');
+
+const { parse } = require('svg-parser');
 
 const data = fs.readFileSync('./blogs-original.csv');
 
 const rows = data.toString().split('\n');
 
 const table = rows
-  .map(row => row.split(','))
-  .filter((row, i) => row.length === 3 && i !== 0);
+  .map(row => row.split(',').map(column => column.trim()))
+  .filter((row, i) => row.length > 1 && i !== 0)
+  .map(row => {
+    if (row[2]) {
+      const feedlyId = encodeURIComponent(`feed/${row[2]}`);
+      row.push(feedlyId);
+    }
+    return row;
+  })
 
-console.log(table);
+console.log(table)
+
+async function getResultAndUpdateREADME() {
+
+  // add count to row
+  for (const row of table) {
+    if (row[4]) {
+      try {
+        const feedlyAPI = `http://cloud.feedly.com/v3/feeds/${row[4]}`;
+        console.log('feedlyAPI', feedlyAPI)
+        const shieldAPI = `https://img.shields.io/badge/dynamic/json?label=count&query=%24.subscribers&url=${encodeURIComponent(feedlyAPI)}`;
+        console.log('shieldAPI', shieldAPI);
+        const res = await axios.get(shieldAPI);
+        const doc = parse(res.data);
+
+        if (doc) {
+          const subscribersCount = doc.children[0].children[3].children[3].children[0].value;
+          row[5] = Number(subscribersCount);
+        } else {
+          row[5] = 0;
+        }
+
+        console.log(row[5])
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  table.sort((a, b) => b[5] - a[5]);
+
+  const newTable = table.map(row => {
+    return [`[![](https://badgen.net/badge/icon/rss?icon=rss&label)](${row[2]})` + (row[5] + '').replace(/\d/g, '*'), row[0], `${row[1]}`, row[3]]
+  })
+  console.log(newTable)
+
+  // let resCsv = 'Introduction, Subscribers, URL\n';
+
+  // newTable.forEach(row => {
+  //   const rowStr = row.join(', ')
+  //   resCsv = resCsv + rowStr + '\n';
+  // })
+
+  // console.log(resCsv);
+
+  // fs.writeFileSync('./blogs-result.csv', resCsv, 'utf8');
+
+
+  // update README
+  const tableContentInMD = markdownTable([['订阅数', '简介', '链接', '标签'], ...newTable]);
+
+  const readmeContent = `# 中文独立博客列表
+
+[![](https://badgen.net/badge/icon/telegram?icon=telegram&label)](https://t.me/indieBlogs) [![](https://badgen.net/badge/icon/patrons/orange?icon=patreon&label)](https://www.patreon.com/timqian)
+
+## 目录
+
+- [博客列表](#博客列表)
+- [什么是独立博客](#什么是独立博客)
+  - [如何提交](#如何提交)
+- [为什么要收集这张列表](#为什么要收集这张列表)
+
+## 博客列表
+
+> 暂时粗暴得按照 feedly 上的订阅数据排了个先后顺序. 关于如何更好得组织博客和推荐内容, 欢迎加入 [Telegram 群](https://t.me/indieBlog) 讨论
+
+${tableContentInMD}
+
+## 什么是独立博客
+
+- 拥有自己的域名
+- 作者本人原创内容
+
+### 如何提交
+
+1. 在 [./blogs-original.csv](./blogs-original.csv) 中填入博客 URL, RSS 及简介
+2. 提交 PR
+3. PR 被 merge 之后本表通过 GitHub action 自动更新
+
+## 为什么要收集这张列表
+
+不止一次听到有人说: “在中国, 独立博客得时代已经过去了”. 确实, 很多博主都转到了公众号, 知乎专栏, 小密圈, 微博... 因为读者比较多; 平台的推荐算法让内容可以被更多人看到; 因为大厂更专业的 SEO (甚至直接与搜索引擎合作😅), 你的内容更容易被搜索到.
+
+但我还是更喜欢独立博客, 因为属于自己的域名, 因为可以自由的排版, 自由的说话.
+
+不得不说, 独立博客在获取新读者方面确实存在问题. 即使你内容再好, 总是需要自己发到各个论坛才能让没有订阅你博客的读者看到你的内容.
+
+是否可以做一个工具, 可以连接这些独立博主, 在保持独立博客的自由得同时, 组织一个独立博客得读者群体, 让独立博客们也有一个稳定的被发现的渠道. 这个工具可能是一个类似之前多说的评论系统, 可能是一个带个性化推荐系统的 RSS 客户端, 可能是一个类似微博, twitter 但是主要内容是独立博客的新东西, 让我们可以知道我们 follow 的博主 follow 了谁...
+
+这个列表是一个开始, 先把独立博客们收集起来, 欢迎加入 [Telegram 群](https://t.me/indieBlogs)一起思考和讨论如何构建这样一个工具.
+  `
+
+  fs.writeFileSync('./README.md', readmeContent, 'utf8');
+ 
+}
+
+getResultAndUpdateREADME()
